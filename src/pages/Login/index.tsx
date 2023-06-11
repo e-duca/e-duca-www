e5bfd32202d43725eeb05b2d298c9ad92768470e
Rodrigo Rodrigues <89035071+RodrigoRodriguesSilvaCampos@users.noreply.github.com>
2023-06-11 17:36:20 -0300

    Feat/loader:Adição do componente loader á tela de login (#6)
    * Feat/loader:Adição do componente loader á tela de login
    
    * Adição de resposta do likert após responde-lo.

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,84 +1,89 @@
-import style from './login.module.scss'
-import { ChangeEvent, useState } from 'react'
-import { FormActions, useForm } from '../../utils/contexts/FormContext'
-import img from '../../assets/img/image08.svg'
-import { Header } from '../SignUp/components/Header'
-import { Modal } from '../../components/Modal'
-import { SignUp } from '../SignUp/components/SignUp'
-import { Button } from '../../components/Button'
-import AuthService from '../../services/auth.service'
-import { useNavigate } from 'react-router-dom'
-import { Input } from '../../components/Input'
-import PostService from '../../services/post.service'
+import style from './login.module.scss';
+import { ChangeEvent, useState } from 'react';
+import { FormActions, useForm } from '../../utils/contexts/FormContext';
+import img from '../../assets/img/image08.svg';
+import { Header } from '../SignUp/components/Header';
+import { Modal } from '../../components/Modal';
+import { SignUp } from '../SignUp/components/SignUp';
+import { Button } from '../../components/Button';
+import AuthService from '../../services/auth.service';
+import { useNavigate } from 'react-router-dom';
+import { Input } from '../../components/Input';
+import PostService from '../../services/post.service';
+import Loaderr from '../../components/Loaderr';
+
 
 export const Login = () => {
-  const navigate = useNavigate()
-  const { state, dispatch } = useForm()
-  const [isModalVisible, setIsModalVisible] = useState<boolean>(false)
-  const [emailError, setEmailError] = useState<boolean>(false)
-  const [passwordError, setPasswordError] = useState<boolean>(false)
-  const [invalid, setInvalid] = useState<boolean>(false)
-
+  const navigate = useNavigate();
+  const { state, dispatch } = useForm();
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const [emailError, setEmailError] = useState<boolean>(false);
+  const [passwordError, setPasswordError] = useState<boolean>(false);
+  const [invalid, setInvalid] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
 
   const data = {
     email: state.email,
     senha: state.password
-  }
+  };
 
   const handleNextStep = async (e: Event) => {
-    e.preventDefault()
-    if (data.email == '' && data.senha == '') {
-      setInvalid(true)
-      return
+    e.preventDefault();
+    if (data.email === '' && data.senha === '') {
+      setInvalid(true);
+      return;
     }
+
+    setLoading(true);
 
     try {
       await AuthService.login(data).then(
         () => {
           PostService.getUser().then(
             (response: any) => {
-              console.log(response.data.perfis[0].nome)
-              if (response.data.perfis[0].nome == 'ROLE_PROFESSOR') {
-                navigate('/professor')
+              console.log(response.data.perfis[0].nome);
+              if (response.data.perfis[0].nome === 'ROLE_PROFESSOR') {
+                navigate('/professor');
               } else {
-                navigate('/estudante')
+                navigate('/estudante');
               }
             },
             (error: any) => {
-              console.log('FORUM/ESTUDANTE/getUser: Erro', error.response)
+              console.log('FORUM/ESTUDANTE/getUser: Erro', error.response);
               if (error.response && error.response.status === 403) {
-                console.log('FORUM/ESTUDANTE/getUser: Erro de autenticação')
+                console.log('FORUM/ESTUDANTE/getUser: Erro de autenticação');
               }
             }
-          )
-
+          );
         },
         (error: any) => {
-          setInvalid(true)
-          console.log(error)
+          setInvalid(true);
+          console.log(error);
         }
-      )
+      );
     } catch (err) {
-      console.log(err)
-      setInvalid(true)
+      console.log(err);
+      setInvalid(true);
+    } finally {
+      setLoading(false);
     }
-  }
+  };
 
   const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
     dispatch({
       type: FormActions.setEmail,
       payload: e.target.value
-    })
-    setEmailError(false)
-  }
+    });
+    setEmailError(false);
+  };
 
   const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
     dispatch({
       type: FormActions.setPassword,
       payload: e.target.value
-    })
-    setPasswordError(false)
-  }
+    });
+    setPasswordError(false);
+  };
 
   return (
     <>
@@ -102,7 +107,7 @@ export const Login = () => {
               onChange={handleEmailChange}
               onBlur={() => {
                 if (data.email === '') {
-                  setEmailError(true)
+                  setEmailError(true);
                 }
               }}
             />
@@ -117,19 +122,22 @@ export const Login = () => {
               onChange={handlePasswordChange}
               onBlur={() => {
                 if (data.senha === '') {
-                  setPasswordError(true)
+                  setPasswordError(true);
                 }
               }}
             />
             <div className={style.error}>
               {passwordError && <p>Por favor, preencha a senha</p>}
-              {invalid && <p>Email ou senha inválidos. Tente novamente!</p>}
+              {invalid && (
+                <p>Email ou senha inválidos. Tente novamente!</p>
+              )}
             </div>
             <Button
               className={style.btnEnter}
               title="Entrar"
               onClick={handleNextStep}
-            ></Button>
+            />
+            {loading && <Loaderr/>}
           </div>
         </div>
         {isModalVisible ? (
@@ -142,5 +150,5 @@ export const Login = () => {
         ) : null}
       </div>
     </>
-  )
-}
+  );
+};
